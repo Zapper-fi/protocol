75315624dac729470da0e607392e3eb1b772b19a
Sébastien Audet <seb.audet26@gmail.com>
2024-06-10 13:18:44 -0400

    update navbar

--- src/modules/Intro/Intro.tsx
+++ src/modules/Intro/Intro.tsx
@@ -9,7 +9,7 @@ export const Intro: React.FC = () => {
 	return (
 		<header
 			style={{
-				padding: "0px 16px 16px 16px",
+				padding: "16px",
 				height: "calc(100vh - 180px)",
 				position: "relative",
 				zIndex: 1,
