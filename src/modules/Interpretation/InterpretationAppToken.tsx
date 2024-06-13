603efe864b6cd8a9350124f7c7ff5e8e1b5521cb
Sam | Zapper <43358952+SamIam-0x@users.noreply.github.com>
2024-06-12 18:43:59 -0700

    Update InterpretationAppToken.tsx

--- src/modules/Interpretation/InterpretationAppToken.tsx
+++ src/modules/Interpretation/InterpretationAppToken.tsx
@@ -44,7 +44,7 @@ export const InterpretationAppToken: React.FC = () => {
 					</h2>
 					<p className="text-alt-color">
 						App Token Interpreters are used to index app-centric token balances
-						for users. This can be loans, deposits, staked amounts, etc... The
+						for users. This can be loans, deposits, pools, etc... The
 						majority of these tokens do not have a market price; rather,
 						they are priced based on the tokens they are redeemable for.
 					</p>
