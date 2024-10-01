e5911febcaee2caee7f5e2b89688f53558cd1027
Christophe Roberge <robergesoftware@gmail.com>
2024-10-01 16:10:47 -0400

    cleanup

--- src/components/ApolloSandboxComponent.tsx
+++ src/components/ApolloSandboxComponent.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { ButtonType, LinkButton } from './LinkButton';
+import { ApolloExplorer } from '@apollo/explorer/react';
 
 const ApolloSandboxComponent: React.FC = () => {
+  const TEST_ADDRESSES = ["0xd8da6bf26964af9d7eed9e03e53415d37aa96045", "0x6f6e75fb472ee39d847d825cc7c9a613e227e261"];
+
   return (
-    <LinkButton
-      type={ButtonType.Primary}
-      buttonCopy="Open Apollo Sandbox"
-      href="https://studio.apollographql.com/sandbox/explorer?endpoint=https://public.zapper.xyz/graphql"
+    <ApolloExplorer
+      graphRef="zapper-public-api@current"
+      persistExplorerState={false}
+      initialState={{
+        document: `query($addresses: [Address!]!) {
+    portfolio(addresses: $addresses) {
+      tokenBalances {
+        address
+        network
+        token {
+          balanceUSD
+        }
+      }
+    }
+  }
+    `,
+        variables: {
+          addresses: TEST_ADDRESSES,
+        },
+        headers: {},
+        displayOptions: {
+          showHeadersAndEnvVars: false,
+          docsPanelState: 'open',
+          theme: 'dark',
+        },
+      }}
+      className="apollo-sandbox"
     />
   );
 };
