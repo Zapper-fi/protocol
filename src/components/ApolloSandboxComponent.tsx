4cb76669327771f26436c900a3aba74d0aabdd03
Christophe Roberge <robergesoftware@gmail.com>
2024-10-02 12:15:57 -0400

    Add public API key

--- src/components/ApolloSandboxComponent.tsx
+++ src/components/ApolloSandboxComponent.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { ApolloExplorer } from '@apollo/explorer/react';
 
 const ApolloSandboxComponent: React.FC = () => {
+  // This is our rate limited, public API key, for free use in the Apollo Sandbox
+  // It is meant to be public
+  const API_KEY = "Basic YWQwMTUyN2UtODEzMy00YTY4LWFkNjctZmJmOGQ5MDQwYWQxOg==";
   const TEST_ADDRESSES = ["0xd8da6bf26964af9d7eed9e03e53415d37aa96045", "0x6f6e75fb472ee39d847d825cc7c9a613e227e261"];
 
   return (
@@ -24,9 +27,11 @@ const ApolloSandboxComponent: React.FC = () => {
         variables: {
           addresses: TEST_ADDRESSES,
         },
-        headers: {},
+        headers: {
+          Authorization: API_KEY,
+        },
         displayOptions: {
-          showHeadersAndEnvVars: true,
+          showHeadersAndEnvVars: false,
           docsPanelState: 'open',
           theme: 'dark',
         },
