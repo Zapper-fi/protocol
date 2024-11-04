58a06cf5fbd9ab98d900a06b83e696e2fb0640cc
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-04 14:25:16 -0500

    client api key

--- src/modules/Dashboard/TransactionHistory.tsx
+++ src/modules/Dashboard/TransactionHistory.tsx
@@ -17,53 +17,55 @@ const QUERY = gql`
 `;
 
 export function TransactionHistory() {
-  const { user } = usePrivy();
-  const { loading, error, data } = useQuery(QUERY, {
-    variables: { privyId: user.id },
-  });
+	const { user } = usePrivy();
+	const { loading, error, data } = useQuery(QUERY, {
+		variables: { privyId: user.id },
+	});
 
-  const { payments } = data?.apiClient || {};
+	const { apiKey, payments } = data?.apiClient || {};
 
-  return (
-    <div className="mb-8">
-      <h2>Transaction History</h2>
+	return (
+		<div className="mb-8">
+			<h2>Transaction History</h2>
 
-      {loading && <p>Loading...</p>}
+			{apiKey && <p>Recent purchases for API key: {apiKey}</p>}
 
-      {error && <p className="text-red-400">Error : {error.message}</p>}
+			{loading && <p>Loading...</p>}
 
-      {!loading && !error && !payments ? (
-        <p>No data found</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Amount</th>
-              <th>Credits Purchased</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {payments?.length === 0 ? (
-              <tr>
-                <td colSpan={4} className="text-center">
-                  No purchases found
-                </td>
-              </tr>
-            ) : (
-              payments?.map((payment) => (
-                <tr key={payment.createdAt}>
-                  <td>{payment.createdAt}</td>
-                  <td>{payment.amount}</td>
-                  <td>{payment.creditsPurchased}</td>
-                  <td>{payment.status}</td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+			{error && <p className="text-red-400">Error: {error.message}</p>}
+
+			{!loading && !error && !payments ? (
+				<p>No data found</p>
+			) : (
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Amount</th>
+							<th>Credits Purchased</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						{payments?.length === 0 ? (
+							<tr>
+								<td colSpan={4} className="text-center">
+									No purchases found
+								</td>
+							</tr>
+						) : (
+							payments?.map((payment) => (
+								<tr key={payment.createdAt}>
+									<td>{payment.createdAt}</td>
+									<td>{payment.amount}</td>
+									<td>{payment.creditsPurchased}</td>
+									<td>{payment.status}</td>
+								</tr>
+							))
+						)}
+					</tbody>
+				</table>
+			)}
+		</div>
+	);
 }
