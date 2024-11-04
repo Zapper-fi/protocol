9349a599fe1ceb733c6c5b424cb6ff56b4eeeb3e
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-04 10:34:26 -0500

    privy

--- src/modules/Dashboard/AuthProvider.tsx
+++ src/modules/Dashboard/AuthProvider.tsx
@@ -2,6 +2,16 @@ import { PrivyProvider } from '@privy-io/react-auth';
 
 const privyAppId = 'cm2ateeqj0531q8pbixyb92qu';
 
+const privyConfig = {
+	appearance: {
+		theme: 'dark',
+	},
+};
+
 export function AuthProvider({ children }) {
-	return <PrivyProvider appId={privyAppId}>{children}</PrivyProvider>;
+	return (
+		<PrivyProvider appId={privyAppId} config={privyConfig}>
+			{children}
+		</PrivyProvider>
+	);
 }
