8e00f720abe1b4882dfac0a88214340df611bf33
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-04 15:16:53 -0500

    update user

--- src/modules/Dashboard/SignInButton.tsx
+++ src/modules/Dashboard/SignInButton.tsx
@@ -1,12 +1,35 @@
-import { usePrivy } from '@privy-io/react-auth';
+import { useLogin } from '@privy-io/react-auth';
 import { Button } from '../../components/Button';
+import { gql, useMutation } from '@apollo/client';
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($privyId: String!, $apiClientName: String!) {
+    updatePrivyId(privyId: $privyId, apiClientName: $apiClientName) {
+      name
+      privyId
+    }
+  }
+`;
 
 export function SignInButton() {
-  const { login } = usePrivy();
+	const [updateUser, { loading, error }] = useMutation(UPDATE_USER);
 
-  return (
-    <Button type="button" variant="primary" onClick={() => login()}>
-      Login
-    </Button>
-  );
+	const { login } = useLogin({
+		onComplete: (user, isNewUser) => {
+			if (isNewUser) {
+				updateUser({
+					variables: {
+						privyId: user.id,
+						apiClientName: user.email.address,
+					},
+				});
+			}
+		},
+	});
+
+	return (
+		<Button type="button" variant="primary" onClick={login}>
+			Login
+		</Button>
+	);
 }
