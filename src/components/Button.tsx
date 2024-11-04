ca7387e4f52f4d433d04ca059f3d1daf9058c260
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-04 14:09:43 -0500

    revert

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 export function Button(props) {
-  const { children, ...rest } = props;
+	const { children, ...rest } = props;
 
-  return (
-    <button
-      className="button--primary border-none rounded-lg px-4 display-flex align-items-center h-12 text-[16px] font-semibold opacity-100 hover:opacity-80 transition-all"
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			className="button--primary cursor-pointer border-none rounded-lg px-4 display-flex align-items-center h-12 text-[16px] font-semibold opacity-100 hover:opacity-80 transition-all"
+			{...rest}
+		>
+			{children}
+		</button>
+	);
 }
