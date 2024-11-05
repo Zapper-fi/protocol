afd4bd40e85061847320779015c4488cf6f6485f
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-05 12:26:36 -0500

    populate

--- src/modules/Dashboard/Debug.tsx
+++ src/modules/Dashboard/Debug.tsx
@@ -1,13 +1,15 @@
 import { usePrivy } from '@privy-io/react-auth';
 
 export function Debug() {
-  const { authenticated, user } = usePrivy();
+  const { ready, user } = usePrivy();
+
+  const value = ready ? JSON.stringify(user, null, 2) : '';
 
   return (
     <details className="mb-8">
       <summary>Debug</summary>
 
-      <textarea rows={10} className="w-full" defaultValue={JSON.stringify(user, null, 2)} />
+      <textarea rows={10} className="w-full" defaultValue={value} />
     </details>
   );
 }
