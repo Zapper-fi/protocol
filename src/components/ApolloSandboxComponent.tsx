332fa9851a8d9fac8dc4908810b84baa8f772578
Christophe Roberge <robergesoftware@gmail.com>
2024-11-08 12:44:48 -0500

    smol

--- src/components/ApolloSandboxComponent.tsx
+++ src/components/ApolloSandboxComponent.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import type React from 'react';
 import { ApolloExplorer } from '@apollo/explorer/react';
+
+type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue };
 
 interface ApolloSandboxComponentProps {
   query: string;
-  variables: Record<string, any>;
+  variables: Record<string, JsonValue>;
 }
 
 const ApolloSandboxComponent: React.FC<ApolloSandboxComponentProps> = ({ query, variables }) => {
   // This is our rate limited, public API key, for free use in the Apollo Sandbox
   // It is meant to be public
-  const API_KEY = "Basic ODRjN2VjZGItM2Y5MS00ZWFjLWFjZDEtYjlkYWVjYjk3MTE1==";
+  const API_KEY = 'Basic ODRjN2VjZGItM2Y5MS00ZWFjLWFjZDEtYjlkYWVjYjk3MTE1==';
 
   return (
     <ApolloExplorer
