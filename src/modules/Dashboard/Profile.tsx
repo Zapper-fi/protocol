6792971fddd94510c416c6ac8552a9e949c53144
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-12 17:04:58 -0500

    link wallet

--- src/modules/Dashboard/Profile.tsx
+++ src/modules/Dashboard/Profile.tsx
@@ -1,6 +1,8 @@
 import { gql } from '@apollo/client';
 import { useAuthQuery } from '@site/src/helpers/useAuthQuery';
 import { Card } from '@site/src/components/Card';
+import { Button } from '@site/src/components/Button';
+import { usePrivy } from '@privy-io/react-auth';
 
 const QUERY = gql`
   query Profile {
@@ -13,6 +15,7 @@ const QUERY = gql`
 
 export function Profile() {
   const { data } = useAuthQuery(QUERY);
+  const { user, linkWallet } = usePrivy();
 
   const { apiKey, name } = data?.apiClientById || {};
 
@@ -20,6 +23,7 @@ export function Profile() {
     <Card>
       <h3>Profile</h3>
       <p>Account Name: {name}</p>
+      <p>Wallet: {user?.wallet?.address || <Button onClick={linkWallet}>Link wallet</Button>}</p>
       <p>API Key: {apiKey}</p>
     </Card>
   );
