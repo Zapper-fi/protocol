55c223d01c70c08dcbea4059de8680635c08dc0b
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-11 23:20:11 -0500

    start auth setup

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { PrivyProvider } from '@privy-io/react-auth';
 
+// TODO: Replace with env variable for development and production
 const privyAppId = 'cls53js9a00lfjw0npsb82scb';
 
 const privyConfig = {
--- src/components/GqlProvider.tsx
+++ src/components/GqlProvider.tsx
@@ -1,21 +1,21 @@
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-const API_KEY = 'ec0888a7-9bd1-4e11-b862-a8d44d157afd';
-
 const httpLink = createHttpLink({
   uri: 'https://api.zapper.xyz/z/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  const authorization = `Basic ${btoa(`${API_KEY}:`)}`;
-  const clientId = localStorage.getItem('zapper.clientId');
+  const token = localStorage.getItem('privy:token');
 
   return {
     headers: {
       ...headers,
-      authorization,
-      'x-zapper-client-id': clientId,
+      // TODO: Get Privy auth token from cookie in the BE then remove this
+      'privy-token': token ? JSON.parse(token) : '',
+
+      // TODO: Remove this once token parsing is implemented
+      authorization: `Basic ${btoa('ec0888a7-9bd1-4e11-b862-a8d44d157afd:')}`,
     },
   };
 });
