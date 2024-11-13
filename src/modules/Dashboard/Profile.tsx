a911b1c6d0248ce3ae9800635933ee4579ea91ce
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-12 20:41:12 -0500

    centering

--- src/modules/Dashboard/Profile.tsx
+++ src/modules/Dashboard/Profile.tsx
@@ -36,9 +36,9 @@ export function Profile() {
 
       <div>
         <div className="flex items-center gap-2">
-          <code className="text-xl font-mono bg-gray-800 p-3 rounded flex-1 overflow-x-auto">{apiKey}</code>
-          <Button type="button" onClick={handleCopyApiKey} className="w-10 px-0 justify-center">
-            <Copy size={24} />
+          <code className="text-base font-mono bg-gray-800 py-2 px-4 rounded flex-1 overflow-x-auto">{apiKey}</code>
+          <Button type="button" onClick={handleCopyApiKey} className="w-10 grid place-content-center">
+            <Copy size={18} />
           </Button>
         </div>
       </div>
