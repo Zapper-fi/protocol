84b88156f2bfd4cc089631ac5f7807a7b8397b0e
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-11-13 11:55:04 -0500

    privy client key

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,7 +1,10 @@
 import { type PrivyClientConfig, PrivyProvider } from '@privy-io/react-auth';
 
 // TODO: Replace with env variable for development and production
+// prod
 const privyAppId = 'cm3dxd29q00lno9q68oqmiaj8';
+// dev
+// const privyAppId = 'cls53js9a00lfjw0npsb82scb';
 
 const privyConfig: PrivyClientConfig = {
   appearance: {
