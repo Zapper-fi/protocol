c61fe7cd5a4be3459251004e77efd1ccfe3802ce
Christophe Roberge <robergesoftware@gmail.com>
2024-11-19 12:41:54 -0500

    Adjust note

--- src/modules/Dashboard/Note.tsx
+++ src/modules/Dashboard/Note.tsx
@@ -7,7 +7,7 @@ export function Note() {
       <div className="flex items-start gap-3">
         <Info className="h-5 w-5 text-primary-default flex-shrink-0" />
         <p className="text-sm">
-          Welcome to the Zapper Protocol Dashboard. While the API is in beta, each client may query up to 10k credits
+          Welcome to the Zapper Protocol Dashboard. While the API is in beta, each client may query up to 5000 credits
           freely, which rounds up to about 1000 requests. Please note that query costs may change during beta.
         </p>
       </div>
