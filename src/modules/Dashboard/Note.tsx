8c12b92e9be810be2e96c0bfcb696bd524fbb6b9
Christophe Roberge <robergesoftware@gmail.com>
2024-11-28 11:37:56 -0500

    smol(studio): Use zapper API naming instead of Protocol

--- src/modules/Dashboard/Note.tsx
+++ src/modules/Dashboard/Note.tsx
@@ -7,7 +7,7 @@ export function Note() {
       <div className="flex items-start gap-3">
         <Info className="h-5 w-5 text-primary-default flex-shrink-0" />
         <p className="text-sm">
-          Welcome to the Zapper Protocol Dashboard. While the API is in alpha, each client may query up to 5,000 credits
+          Welcome to the Zapper API Dashboard. While the API is in alpha, each client may query up to 5,000 credits
           for free, providing at least 1,500 queries to test the API. Please note that query costs may change in the future.
         </p>
       </div>
