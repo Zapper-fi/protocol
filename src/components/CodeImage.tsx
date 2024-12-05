60d9634ddedef2e6ac152bc49e7d2382c2e4328a
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-05 17:14:31 -0500

    override that margin

--- src/components/CodeImage.tsx
+++ src/components/CodeImage.tsx
@@ -4,7 +4,9 @@ export function CodeImage({ src, alt, children }) {
   return (
     <div className="relative">
       <div className="h-[400px] rounded-3xl grid overflow-y-auto">
-        <CodeBlock language="graphql">{children}</CodeBlock>
+        <CodeBlock language="graphql" className="!mb-0">
+          {children}
+        </CodeBlock>
       </div>
       <img src={src} alt={alt} className="absolute top-8 right-8 h-[calc(100%-2rem)]" />
     </div>
