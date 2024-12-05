6240047d04f7b28de063d3d7f0fbf8a429022c08
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-05 15:57:46 -0500

    rounded code block

--- src/components/CodeImage.tsx
+++ src/components/CodeImage.tsx
@@ -1,7 +1,7 @@
 export function CodeImage({ src, alt, children }) {
   return (
     <div className="relative">
-      <div className="h-[400px] grid overflow-y-auto">{children}</div>
+      <div className="h-[400px] rounded-3xl grid overflow-y-auto ">{children}</div>
       <img src={src} alt={alt} className="absolute bottom-0 right-4 h-full" />
     </div>
   );
