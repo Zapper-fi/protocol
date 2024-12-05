920ca6b6e13c55707384170e17800458cfa57e42
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-05 15:08:41 -0500

    img full height

--- src/components/CodeImage.tsx
+++ src/components/CodeImage.tsx
@@ -2,7 +2,7 @@ export function CodeImage({ src, alt, children }) {
   return (
     <div className="relative">
       <div className="h-[400px] grid overflow-y-auto">{children}</div>
-      <img src={src} alt={alt} className="absolute bottom-0 right-4 w-72" />
+      <img src={src} alt={alt} className="absolute bottom-0 right-4 h-full" />
     </div>
   );
 }
