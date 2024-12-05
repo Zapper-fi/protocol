621d27a609b22c03b16495567e9d3834a8ffeb70
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-05 16:20:08 -0500

    code blocks

--- src/components/CodeImage.tsx
+++ src/components/CodeImage.tsx
@@ -1,7 +1,11 @@
+import CodeBlock from '@theme/CodeBlock';
+
 export function CodeImage({ src, alt, children }) {
   return (
     <div className="relative">
-      <div className="h-[400px] rounded-3xl grid overflow-y-auto ">{children}</div>
+      <div className="h-[400px] rounded-3xl grid overflow-y-auto">
+        <CodeBlock language="graphql">{children}</CodeBlock>
+      </div>
       <img src={src} alt={alt} className="absolute bottom-0 right-4 h-full" />
     </div>
   );
