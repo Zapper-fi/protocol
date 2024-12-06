2dc082aafdb5da1c2e078ffaa76d1c8406dec6b3
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-06 09:45:17 -0500

    responsive tweaks

--- src/components/CodeImage.tsx
+++ src/components/CodeImage.tsx
@@ -8,7 +8,7 @@ export function CodeImage({ src, alt, children }) {
           {children}
         </CodeBlock>
       </div>
-      <img src={src} alt={alt} className="absolute top-8 right-8 h-[calc(100%-2rem)]" />
+      <img src={src} alt={alt} className="absolute bottom-0 right-0 md:top-8 md:right-8 h-1/2 md:h-[calc(100%-2rem)]" />
     </div>
   );
 }
