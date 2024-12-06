f854e96aede48612ece68cf3c8a3003bccc18c1b
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-06 12:08:31 -0500

    email support (#145)

--- src/modules/Dashboard/SignInButton.tsx
+++ src/modules/Dashboard/SignInButton.tsx
@@ -52,7 +52,7 @@ export function SignInButton() {
         },
         {
           label: 'Contact Support',
-          href: 'https://help.zapper.xyz/hc/en-us',
+          href: 'mailto:api@zapper.xyz?subject=Customer%20Support',
         },
         {
           label: 'Sign Out',
