016f53007df03e1b7c221f632d48ae05ec661a7b
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-09 15:27:40 -0500

    min width

--- src/modules/Dashboard/BuyCredits.tsx
+++ src/modules/Dashboard/BuyCredits.tsx
@@ -262,7 +262,7 @@ export function BuyCredits() {
               onChange={handlePointsChange}
               onBlur={handleBlur}
               min={MIN_POINTS}
-              className="bg-transparent border-none text-center text-lg field-sizing-content"
+              className="bg-transparent border-none text-center text-lg field-sizing-content min-w-28"
               placeholder="Enter credits amount"
             />
             <button
