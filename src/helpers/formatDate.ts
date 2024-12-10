469f3f3adf998ead93043a4922a2873bfefc7c83
Francis Nepomuceno <n3ps@users.noreply.github.com>
2024-12-10 15:37:23 -0500

    chore(docs): Tweak payment history table

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,10 +1,11 @@
 export function formatDate(dateString) {
   const options = {
     year: 'numeric',
-    month: 'long',
+    month: 'short',
     day: 'numeric',
-    hour: '2-digit',
+    hour: 'numeric',
     minute: '2-digit',
+    hour12: true,
   };
   return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
 }
